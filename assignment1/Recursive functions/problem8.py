f7028ca40629f4d4167b762ede91d6ec517dc6ce
Zoe Rochelle <zgrochelle@gmail.com>
2021-09-03 16:06:50 -0400

    8th batch

--- assignment1/Recursive functions/problem8.py
+++ assignment1/Recursive functions/problem8.py
@@ -4,54 +4,28 @@
 #time complexity:
 #space complexity:
 
-class Solution:
-	def __init__(self):
-		self.largest = ""
-	def check(self,p,q,m):
-		st = ""
-		while p <= q:
-			if (m[p] != m[q]):
-				return False
-			elif p == q:
-				st = st + m[p] + st[::-1]
-			else:
-				st = st + m[q]
-			p+=1
-			q-=1
-			
-		if len(st) >= len(self.largest):
-			self.largest = st
-
-	def isPalindrome(self, s):
-		new_str = s.replace(' ', '')
-		
-		# i = 0
-		# j = len(new_str)-1
-		# while i <= j:
-		# 	if new_str[i] == new_str[j]:
-		# 		self.check(i,j,new_str)
-		# 		print(new_str[i],i,"-",new_str[j],j)
-		# 	i+=1
-		# 	j-=1
-
-		size = len(new_str)
-		for i in range(size):
-			for j in range(size-1,0+i,-1):
-				if new_str[i] == new_str[j]:
-					print(new_str[i],i,"-",new_str[j],j)
-
-					self.check(i,j,new_str)
-		return self.largest
-
-s = Solution()
+#iterative approach
+class Solution1:
+    def __init__(self):
+        self.largest = ""
+    def check(self,p,q,m):
+        s = ""
+        for each in range(p,q+1):
+            s = s + m[each]
+        if s == s[::-1]:
+            if len(s) >= len(self.largest):
+                self.largest = s
+    def isPalindrome(self, s):
+        new_str = s.replace(' ', '')
+        size = len(new_str)
+        for i in range(size):
+            for j in range(size-1,0+i,-1):
+                if new_str[i] == new_str[j]:
+                    self.check(i,j,new_str)
+        return self.largest
+s = Solution1()
 
 test_string = "ytuxiiqaaqiixytu"
 print(s.isPalindrome(test_string))
 
-# test_string = "ytuxiiqaaqiixytu"
-# expected "xiiqaaqiix"
-# produces "xiiqa"
-
-# test_string = "abaxyygyzzygxgyzyg "
-# expected "zygxgyz"
-# produces "zygxgyz"
+#recursive approach
